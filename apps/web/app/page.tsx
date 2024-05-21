e2ed1effc3d5690733d303a0e266f8029afa11ce
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2024-05-21 10:57:29 +0200

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,31 +4,6 @@ import { Board, Button } from "@repo/ui";
 
 import styles from "../styles/index.module.css";
 
-import goku from "./../assets/images/goku.jpg";
-import vegita from "./../assets/images/vegita.jpg";
-import bulma from "./../assets/images/bulma.jpg";
-import beerus from "./../assets/images/beerus.jpg";
-import buu from "./../assets/images/buu.jpg";
-import haiya from "./../assets/images/haiya.jpg";
-import picolo from "./../assets/images/picolo.jpg";
-import frieza from "./../assets/images/frieza.jpg";
-import mutenroshi from "./../assets/images/mutenroshi.jpg";
-import pilaf from "./../assets/images/pilaf.jpg";
-import CardBackSrc from "./../assets/images/cardBack.jpg";
-
-const imageList = [
-  goku,
-  vegita,
-  bulma,
-  beerus,
-  buu,
-  haiya,
-  picolo,
-  frieza,
-  mutenroshi,
-  pilaf,
-];
-
 import { useEffect, useState } from "react";
 
 export default function Web() {
@@ -39,13 +14,8 @@ export default function Web() {
   }, []);
   return (
     <div className={styles.container}>
-      <h1>Wes</h1>
-      <Button onClick={() => console.log("Pressed!")} text="Boop" />
-      <Board
-        imageList={imageList}
-        cardBackSrc={CardBackSrc}
-        screenWidth={screenWidth}
-      />
+      <h1>Web</h1>
+      <Button onClick={() => console.log("Pressed!")} text="Web button" />
     </div>
   );
 }
