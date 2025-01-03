c3046e949071f7cb079cf7a747a8ccd8da8d5da7
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2025-01-03 15:24:08 +0100

    supabase mock

--- packages/ui/src/screens/HomePage/HomePage.test.tsx
+++ packages/ui/src/screens/HomePage/HomePage.test.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { HomePage } from "./HomePage";
 import { renderWithProviders } from "../../utils/testUtils";
-import { fireEvent, screen } from "@testing-library/react-native";
+import { fireEvent, screen, waitFor } from "@testing-library/react-native";
 
-it("presses 1 player button then cancel", () => {
+jest.useFakeTimers();
+
+it("presses 1 player button then cancel", async () => {
   renderWithProviders(<HomePage />);
   const { getByText, queryByText } = screen;
 
   fireEvent.press(getByText("1 player"));
-  expect(getByText("Enter your name")).toBeOnTheScreen();
-  fireEvent.press(getByText("Cancel"));
+
+  await waitFor(() => {
+    expect(getByText("Enter your name")).toBeOnTheScreen();
+    fireEvent.press(getByText("Cancel"));
+  });
 
   expect(queryByText("Cancel")).not.toBeOnTheScreen();
 });
@@ -20,20 +25,25 @@ it("presses 1 player button then confirm", async () => {
 
   expect(queryByTestId("cardContainer-0")).not.toBeOnTheScreen();
   fireEvent.press(getByText("1 player"));
-  const input = getByTestId("player1Name");
-  fireEvent.changeText(input, "test name");
-  fireEvent.press(getByText("Confirm"));
 
-  expect(await findByTestId("cardContainer-0")).toBeOnTheScreen();
+  await waitFor(async () => {
+    const input = getByTestId("player1Name");
+    fireEvent.changeText(input, "test name");
+    fireEvent.press(getByText("Confirm"));
+    expect(await findByTestId("cardContainer-0")).toBeOnTheScreen();
+  });
 });
 
-it("presses 2 players button then cancel", () => {
+it("presses 2 players button then cancel", async () => {
   renderWithProviders(<HomePage />);
   const { getByText, queryByText } = screen;
 
   fireEvent.press(getByText("2 players"));
-  expect(getByText("Enter your names")).toBeOnTheScreen();
-  fireEvent.press(getByText("Cancel"));
+
+  await waitFor(() => {
+    expect(getByText("Enter your names")).toBeOnTheScreen();
+    fireEvent.press(getByText("Cancel"));
+  });
 
   expect(queryByText("Cancel")).not.toBeOnTheScreen();
 });
@@ -45,12 +55,14 @@ it("presses 2 players button then confirm", async () => {
   expect(queryByTestId("cardContainer-0")).not.toBeOnTheScreen();
   fireEvent.press(getByText("2 players"));
 
-  const input1 = getByTestId("player1Name");
-  const input2 = getByTestId("player2Name");
+  await waitFor(async () => {
+    const input1 = getByTestId("player1Name");
+    const input2 = getByTestId("player2Name");
 
-  fireEvent.changeText(input1, "test name 1");
-  fireEvent.changeText(input2, "test name 2");
-  fireEvent.press(getByText("Confirm"));
+    fireEvent.changeText(input1, "test name 1");
+    fireEvent.changeText(input2, "test name 2");
+    fireEvent.press(getByText("Confirm"));
 
-  expect(await findByTestId("cardContainer-0")).toBeOnTheScreen();
+    expect(await findByTestId("cardContainer-0")).toBeOnTheScreen();
+  });
 });
