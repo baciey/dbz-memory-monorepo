6e99094a5ccacc529adc382642cdf5c2e259f1dd
baciey <baciey@gmail.com>
2025-01-09 21:09:54 +0100

    images, icons change

--- apps/expo-app/app/(tabs)/index.tsx
+++ apps/expo-app/app/(tabs)/index.tsx
@@ -1,6 +1,28 @@
 import { ScreenWrapper } from "@/components/ScreenWrapper";
 import { HomePage } from "@repo/ui";
 
+// https://www.deviantart.com/kosejhesi/gallery
+// https://creativecommons.org/licenses/by/3.0/
+// https://www.deviantart.com/kosejhesi/art/Piccolo-1128480508
+
+// https://www.deviantart.com/zika-arts/gallery
+// https://creativecommons.org/licenses/by-nd/3.0/
+// https://www.deviantart.com/zika-arts/art/Kid-Buu-596778494
+// https://www.deviantart.com/zika-arts/art/Andorid18-596777908
+// https://www.deviantart.com/zika-arts/art/Future-Trunks-597998239
+// https://www.deviantart.com/zika-arts/art/Angry-Cell-powering-up-620097179
+// https://www.deviantart.com/zika-arts/art/Vegeta-Saiyan-Normal-615088989
+// https://www.deviantart.com/zika-arts/art/Andriod17-fanart-Remake-946471934
+
+// https://www.deviantart.com/shady0da/gallery
+// https://creativecommons.org/licenses/by-sa/3.0/
+// https://www.deviantart.com/shady0da/art/SS-Goku-PFP-676409925
+// https://www.deviantart.com/shady0da/art/Frieza-PFP-676390496
+
+// https://www.deviantart.com/luimuu/gallery
+// https://creativecommons.org/licenses/by/3.0/
+// https://www.deviantart.com/luimuu/art/Kid-Gohan-772771232
+
 export default function Home() {
   return (
     <ScreenWrapper>
