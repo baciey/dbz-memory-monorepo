1c0ff68faf0c5ed50a8beb4fe7146f88c1be1562
Maciej Baczynski <maciej.baczynski@ordergroup.pl>
2025-02-04 15:43:12 +0100

    contact page tests

--- packages/ui/src/screens/ContactPage/ContactPage.test.tsx
+++ packages/ui/src/screens/ContactPage/ContactPage.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { renderWithProviders } from "../../utils/testUtils";
-import { screen, waitFor } from "@testing-library/react-native";
+import { fireEvent, screen, waitFor } from "@testing-library/react-native";
 import { ContactPage } from "./ContactPage";
 
 jest.useFakeTimers();
@@ -11,5 +11,48 @@ it("renders correctly", async () => {
 
   await waitFor(() => {
     expect(getByText("Contact us")).toBeOnTheScreen();
+  });
+});
+
+it("sends email with correct data", async () => {
+  renderWithProviders(<ContactPage />);
+  const { getAllByText, getAllByTestId, getByTestId, queryByText } = screen;
+
+  await waitFor(async () => {
+    expect(getAllByText("Your email")).toHaveLength(2);
+    expect(getAllByText("Message")).toHaveLength(2);
+    expect(getAllByText("Send")).toHaveLength(1);
+  });
+
+  const inputs = getAllByTestId("text-input-flat-label-active");
+
+  fireEvent.changeText(inputs[0], "test@email.com");
+  fireEvent.changeText(inputs[1], "message");
+  fireEvent.press(getByTestId("send-button"));
+
+  await waitFor(async () => {
+    expect(queryByText("This field cannot be empty.")).not.toBeVisible();
+  });
+});
+
+it("does not send email with incorrect data", async () => {
+  renderWithProviders(<ContactPage />);
+  const { getAllByText, getAllByTestId, getByTestId, queryByText } = screen;
+
+  await waitFor(async () => {
+    expect(getAllByText("Your email")).toHaveLength(2);
+    expect(getAllByText("Message")).toHaveLength(2);
+    expect(getAllByText("Send")).toHaveLength(1);
+  });
+
+  const inputs = getAllByTestId("text-input-flat-label-active");
+
+  fireEvent.changeText(inputs[0], "test@email");
+  fireEvent.changeText(inputs[1], "");
+  fireEvent.press(getByTestId("send-button"));
+
+  await waitFor(async () => {
+    expect(queryByText("This field cannot be empty.")).toBeVisible();
+    expect(queryByText("Invalid email address format.")).toBeVisible();
   });
 });
